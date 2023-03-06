eb8a31adfa04f67160a7128a823e92330d12da08
Simon-W1 <43935421+Simon-W1@users.noreply.github.com>
2023-03-06 19:29:08 +0100

    Added computer intelligence

--- application/computer.py
+++ application/computer.py
@@ -1,5 +1,16 @@
 """Computer class."""
+from player import Player
 
 
-class ComputerPlayer:
+class ComputerPlayer(Player):
     """Computer class."""
+
+    def __init__(self):
+        """Initialize computer player."""
+        super().__init__("Computer")
+
+    def choose_move(self):
+        """Choose whether to roll or hold."""
+        if self.turn_score >= 20 or self.turn_score + self.score >= 100:
+            return "hold"
+        return "roll"
