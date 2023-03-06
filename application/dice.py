234fa71c7343f78a8f2b81c4fb3b5f3938d3d9af
hansson0950 <emil.hansson@live.se>
2023-03-06 15:22:29 +0100

    Dice and player classes finished

--- application/dice.py
+++ application/dice.py
@@ -1,5 +1,10 @@
 """Dice class."""
+import random
 
 
 class Dice:
     """Dice class."""
+
+    def roll(self):
+        """Roll the die."""
+        return random.randint(1, 6)
--- application/player.py
+++ application/player.py
@@ -1,5 +1,34 @@
 """Player class."""
+from dice import Dice
+from ui import UI
 
 
 class Player:
     """Player class."""
+
+    def __init__(self, name):
+        """Initialize the player."""
+        self.name = name
+        self.turn_score = 0
+        self.total_turns = 0
+        self.score = 0
+        self.dice = Dice()
+        self.ui = UI()  # pylint: disable-msg=C0103
+
+    def roll_dice(self):
+        """Roll the die."""
+        roll = self.dice.roll()
+        self.ui.display_roll(roll)
+        return roll
+
+    def add_to_score(self, points):
+        """Add points to score."""
+        self.score = points
+
+    def add_to_turn(self, points):
+        """Add points to current turn."""
+        self.turn_score += points
+
+    def end_turn(self):
+        """End current turn."""
+        self.ui.display_turn_end(self.name)
