1c39d299b4c719b3c2b6b337a5a4a552b96fde5e
hansson0950 <emil.hansson@live.se>
2023-03-08 23:57:50 +0100

    Improving comments on test_ui.py

--- unit_tests/test_ui.py
+++ unit_tests/test_ui.py
@@ -20,13 +20,13 @@ class UITest(unittest.TestCase):
         self.ui = UI()  # pylint: disable-msg=C0103
 
     def test_display_welcome(self):
-        """Display welcome message."""
+        """Test display welcome message."""
         with patch("sys.stdout", new=StringIO()) as fake_out:
             self.ui.display_welcome()
             self.assertEqual(fake_out.getvalue().strip(), "Welcome to Pig!")
 
     def test_display_scores(self):
-        """Display scores in a two-player-game."""
+        """Test display scores in a two-player-game."""
         player1 = Player("John")
         player2 = Player("Rob")
         with patch("sys.stdout", new=StringIO()) as fake_out:
@@ -36,20 +36,20 @@ class UITest(unittest.TestCase):
                 "---------------\n\nCurrent Scores:\nJohn: 0\nRob: 0")
 
     def test_display_roll(self):
-        """Display the roll."""
+        """Test display roll."""
         with patch("sys.stdout", new=StringIO()) as fake_out:
             self.ui.display_roll(3)
             self.assertEqual(fake_out.getvalue().strip(), "Roll: 3")
 
     def test_display_bust(self):
-        """Display when a one is rolled."""
+        """Test display when a one is rolled."""
         with patch("sys.stdout", new=StringIO()) as fake_out:
             self.ui.display_bust()
             self.assertEqual(fake_out.getvalue().strip(),
                              "Bust! Your turn is over.")
 
     def test_display_ask_roll_again(self):
-        """Display "Roll again?"."""
+        """Test display "Roll?"."""
         with patch("builtins.input", return_value="y"):
             self.assertEqual(self.ui.ask_roll_again(), "y")
         with patch("builtins.input", return_value="n"):
@@ -58,14 +58,14 @@ class UITest(unittest.TestCase):
             self.assertEqual(self.ui.ask_roll_again(), "cheat")
 
     def test_display_turn(self):
-        """Display turns."""
+        """Test display turns."""
         player = Player("John")
         with patch("sys.stdout", new=StringIO()) as fake_out:
             self.ui.display_turn(player.name)
             self.assertEqual(fake_out.getvalue().strip(), "John's turn!")
 
     def test_display_turn_end(self):
-        """Display turn end."""
+        """Test display turn end."""
         player = Player("John")
         with patch("sys.stdout", new=StringIO()) as fake_out:
             self.ui.display_turn_end(player.name)
@@ -73,13 +73,13 @@ class UITest(unittest.TestCase):
                              "John's turn is over.")
 
     def test_display_game_over(self):
-        """Display game over."""
+        """Test display game over."""
         with patch("sys.stdout", new=StringIO()) as fake_out:
             self.ui.display_game_over()
             self.assertEqual(fake_out.getvalue().strip(), "Game over!")
 
     def test_display_high_scores(self):
-        """Display high scores."""
+        """Test display high scores."""
         high_score = HighScore()
         high_score.is_high_score(5, "John")
         high_score.is_high_score(8, "Rob")
@@ -89,7 +89,7 @@ class UITest(unittest.TestCase):
                              "High Scores:\nJohn: 5\nRob: 8")
 
     def test_display_rules(self):
-        """Display rules."""
+        """Test display rules."""
         # pylint: disable-msg=C0301
         expected_output = "Rules:\n- Each turn, a player repeatedly rolls a die until either a 1 is rolled or the player decides to hold.\n- If the player rolls a 1, they score nothing and their turn ends.\n- If the player rolls any other number, it is added to their turn total and the player's turn continues.\n- If the player chooses to hold, their turn total is added to their score, and it becomes the next player's turn.\n- The first player to score 100 or more points wins."  # noqa
         with patch("sys.stdout", new=StringIO()) as fake_out:
@@ -97,24 +97,24 @@ class UITest(unittest.TestCase):
             self.assertEqual(fake_out.getvalue().strip(), expected_output)
 
     def test_ask_player_name(self):
-        """Display player name."""
+        """Test ask player name."""
         with patch("builtins.input", return_value="John"):
             self.assertEqual(self.ui.ask_player_name(1), "John")
 
     def test_display_menu(self):
-        """Display menu options."""
+        """Test display menu options."""
         with patch("builtins.input", return_value="2"):
             self.assertEqual(self.ui.display_menu(), "2")
 
     def test_ask_player_amount(self):
-        """Display player amount."""
+        """Test ask player amount."""
         with patch("builtins.input", return_value="1"):
             self.assertEqual(self.ui.ask_roll_again(), "1")
         with patch("builtins.input", return_value="2"):
             self.assertEqual(self.ui.ask_roll_again(), "2")
 
     def test_display_turn_score(self):
-        """Display turn score."""
+        """Test display turn score."""
         with patch("sys.stdout", new=StringIO()) as fake_out:
             self.ui.display_turn_score(3)
             self.assertEqual(fake_out.getvalue().strip(),
